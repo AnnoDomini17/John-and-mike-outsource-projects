4ea92571cff5f55a1c8ba752e6789a8f0b87a860
michael <124746912+ghelo2128@users.noreply.github.com>
2024-09-14 14:12:33 +0800

    update arrow code and css

--- William Flaiz-Sports and Nature Gear/#173802-Custom Development-Navigation widget/script.js
+++ William Flaiz-Sports and Nature Gear/#173802-Custom Development-Navigation widget/script.js
@@ -85,7 +85,9 @@
             let $parentItem = $(`
                 <li>
                     <a href="${categoryData.categoryLink}">${category}</a>
-                    <span class="arrow">&#x25BC;</span>
+                    <span class="icon-angle-down">
+                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="800px" height="800px" viewBox="0 -4.5 20 20" version="1.1"> <title>arrow_down [#338]</title> <desc>Created with Sketch.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Dribbble-Light-Preview" transform="translate(-220.000000, -6684.000000)" fill="#000000"> <g id="icons" transform="translate(56.000000, 160.000000)"> <path d="M164.292308,6524.36583 L164.292308,6524.36583 C163.902564,6524.77071 163.902564,6525.42619 164.292308,6525.83004 L172.555873,6534.39267 C173.33636,6535.20244 174.602528,6535.20244 175.383014,6534.39267 L183.70754,6525.76791 C184.093286,6525.36716 184.098283,6524.71997 183.717533,6524.31405 C183.328789,6523.89985 182.68821,6523.89467 182.29347,6524.30266 L174.676479,6532.19636 C174.285736,6532.60124 173.653152,6532.60124 173.262409,6532.19636 L165.705379,6524.36583 C165.315635,6523.96094 164.683051,6523.96094 164.292308,6524.36583" id="arrow_down-[#338]"> </path> </g> </g> </g> </svg>
+                    </span>
                 </li>`
             );
             let $childList = $('<ul class="child"></ul>');
@@ -109,7 +111,7 @@
 
     function setupEventHandlers() {
         // Handle arrow clicks
-        $('.sidenav').on('click', '.arrow', function(e) {
+        $('.sidenav').on('click', '.icon-angle-down', function(e) {
             e.stopPropagation();
             let $parentItem = $(this).closest('li');
             let $childList = $parentItem.find('.child');
@@ -123,9 +125,9 @@
         });
 
         // Add hover effects
-        $('.sidenav').on('mouseenter', '.arrow', function() {
+        $('.sidenav').on('mouseenter', '.icon-angle-down', function() {
             $(this).addClass('hoverSidenavtext');
-        }).on('mouseleave', '.arrow', function() {
+        }).on('mouseleave', '.icon-angle-down', function() {
             $(this).removeClass('hoverSidenavtext');
         });
     }
